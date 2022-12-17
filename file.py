f0a63484bc7ce3f6f46968cad88fdbee24579b62
vyshnavi-18 <106963448+vyshnavi-18@users.noreply.github.com>
2022-12-17 11:10:50 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,24 @@
+# # # # file=open("nandu.txt","w")
+# # # # file.write("hi,how are you")
+# # # # import os
+# # # # # os.rename("sunitha.txt","ram.txt")
+# # # # os.remove("vaishu,txt")
+# # # # # file.rename("vaishu,txt")
+# # # # print(1 and True)
+# # # a={'name','class','row'}
+# # # c=dict.fromkeys(a)
+# # # print(c)
+# # # print(type(a))
+# # a={'name':9,'age':90}
+# # b=a.setdefault("age",0)
+# # print(b)
+
+# l=int(input("enter len of the dict:"))
+# d={}
+# i=0
+# while i<=l:
+#     k=input("enter any key:")
+#     v=input("enter the value:")
+#     d[k]=v
+#     i+=1
+# print(d)
---
+++ meraky.py
@@ -0,0 +1,68 @@
+file=open("delhi.txt","w")
+file.write("neela\nsarika\nharshad\ntrishna\nshekar\nnand\nanoop")
+file.close()
+# priyanka - shimla
+# neela - delhi
+# sashi - jaipur
+# sarika - delhi
+# deepti - shimla
+# harshad - delhi
+# trishna - delhi
+# pradeep - jaipur
+# sekhar - delhi
+# nand - delhi
+# anoop - delhi
+# balwinder - jaipur
+
+file1=open("people1.txt","w")
+file1.write("i completed my diploma engeneering")
+file1.close() 
+
+# file2=open("people1.txt","a")
+# file2.write("\ni completed my software courses also")
+# file2.close()
+# my_file = open("people1.txt","r")
+# file_data = my_file.read()
+# print (file_data)
+# # my_file.close()
+
+# my_file2 = open("people2.txt", "w")
+# my_file2.write("Abhishek - Gurgaon")
+# my_file2.write("Ranveer - Delhi")
+# my_file2.close()
+
+
+# print ("AbhishekRanveer")
+# print ("---------------------------------")
+# print ("Abhishek\nRanveer")
+# print ("---------------------------------")
+# print ("Abhishek\n\nRanveer")
+# print ("---------------------------------")
+
+
+# my_file3 = open("people3.txt", "w")
+# my_file3.write("Abhishek - Gurgaon\n")
+# my_file3.write("Ranveer - Delhi")
+# my_file3.close()
+
+
+# batch1_students = ["Shivam", "Rahul", "Kavay", "Dhannu", "Deepanshu", "Nitin", "Manoj", "Shakrudin", "Tara", "Suraj", "Krishna"]
+# students_file = open("navgurukul_students.html", "w")
+# students_file.write("vaishu\n")
+# students_file.write("anand\n")
+# students_file.write("nandhu\n")
+# students_file.write("sweetie\n")
+# students_file.write("abbayi\n")
+# students_file.write("ammayi\n")
+# for student in batch1_students:
+#     students_file.write("" + student + "\n")
+# students_file.write("vaishu\n")
+# students_file.write("anand\n")
+# students_file.write("nandu\n")
+# students_file.close()
+
+
+# my_file3 = open("vaishnavi.txt", "w")
+# my_file3.write("Yahan main kuch likha\n sreekanya")
+# my_file3.write("\nYaha maine kuch aur bhi likha.")
+
